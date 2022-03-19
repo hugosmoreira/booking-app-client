d655d8770e67d7d79b6ce8c48a05438df715e677
hugosmoreira <hugo.n4@gmail.com>
2022-03-18 19:08:27 -0700

    order modal update stripecucess

--- src/actions/hotel.js
+++ src/actions/hotel.js
@@ -18,11 +18,12 @@ export const diffDays = (from, to) => {
   return difference;
 };
 
-export const sellerHotels = async (token) => await axios.get(`${process.env.REACT_APP_API}/seller-hotels`, {
+export const sellerHotels = async (token) =>
+  await axios.get(`${process.env.REACT_APP_API}/seller-hotels`, {
     headers: {
-        Authorization: `Bearer ${token}`,
-    }
-})
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
 export const deleteHotel = async (token, hotelId) =>
   await axios.delete(`${process.env.REACT_APP_API}/delete-hotel/${hotelId}`, {
@@ -31,9 +32,10 @@ export const deleteHotel = async (token, hotelId) =>
     },
   });
 
-  export const read = async (hotelId) => await axios.get(`${process.env.REACT_APP_API}/hotel/${hotelId}`);
+export const read = async (hotelId) =>
+  await axios.get(`${process.env.REACT_APP_API}/hotel/${hotelId}`);
 
-  export const updateHotel = async (token, data, hotelId) =>
+export const updateHotel = async (token, data, hotelId) =>
   await axios.put(
     `${process.env.REACT_APP_API}/update-hotel/${hotelId}`,
     data,
@@ -43,3 +45,17 @@ export const deleteHotel = async (token, hotelId) =>
       },
     }
   );
+
+export const userHotelBookings = async (token) =>
+  await axios.get(`${process.env.REACT_APP_API}/user-hotel-bookings`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const isAlreadyBooked = async (token, hotelId) =>
+  await axios.get(`${process.env.REACT_APP_API}/is-already-booked/${hotelId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
